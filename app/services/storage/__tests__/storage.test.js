a986dc4ccb48ce3a912ae0602e1dab19f3d4515b
Imran Ariffin <ariffin.imran@gmail.com>
2019-12-17 23:35:59 -0500

    Fix db connection auto-close & re-use

--- app/services/storage/__tests__/storage.test.js
+++ app/services/storage/__tests__/storage.test.js
@@ -18,16 +18,21 @@ jest.mock('react-native-sqlite-storage', () => {
     openDatabase: jest.fn(() => Promise.resolve())
   }
 })
+jest.useFakeTimers()
 
 describe('storage service setup', () => {
   beforeEach(() => {
-    SQLite.openDatabase.mockClear()
+    const db = { close: jest.fn() }
+    SQLite.openDatabase = jest.fn(() => Promise.resolve(db))
     logger.log.mockClear()
+    jest.runAllTimers()
+    jest.spyOn(global, 'setTimeout').mockClear()
+    jest.spyOn(global, 'clearTimeout').mockClear()
   })
 
   test('setup successfully', async () => {
-    SQLite.openDatabase = jest.fn(() => Promise.resolve('some-db'))
-
+    const mockDb = { close: jest.fn() }
+    SQLite.openDatabase = jest.fn(() => Promise.resolve(mockDb))
     await storage.setup()
 
     expect(SQLite.openDatabase.mock.calls).toEqual([
@@ -40,8 +45,26 @@ describe('storage service setup', () => {
       ]
     ])
     expect(logger.log.mock.calls).toEqual([
-      ['Opened db connection: \'some-db\'.', 'Lasts for at least', '15000 ms']
+      [`Opened db connection: '${mockDb}'.`, 'Lasts for at least', '15000 ms']
     ])
+  })
+
+  test('get db connection successfully', async () => {
+    const mockDb = { close: jest.fn() }
+    SQLite.openDatabase = jest.fn(() => Promise.resolve(mockDb))
+
+    const db = await storage.getDb()
+
+    expect(SQLite.openDatabase.mock.calls).toEqual([
+      [
+        {
+          androidDatabaseProvider: 'system',
+          location: 'default',
+          name: 'small-steps-test.db'
+        }
+      ]
+    ])
+    expect(db).toEqual(mockDb)
   })
 
   test('setup fails', async () => {
@@ -49,5 +72,42 @@ describe('storage service setup', () => {
     SQLite.openDatabase = jest.fn(() => Promise.reject(error))
 
     await expect(storage.setup()).rejects.toThrow(error)
+  })
+
+  test('get db connection fails', async () => {
+    const error = Error('some-error')
+    SQLite.openDatabase = jest.fn(() => Promise.reject(error))
+
+    await expect(storage.getDb()).rejects.toThrow(error)
+  })
+
+  test('auto-close db connection within a fixed time if unused', async () => {
+    const db = { close: jest.fn() }
+    SQLite.openDatabase = jest.fn(() => Promise.resolve(db))
+
+    await storage.setup({ timeout: 100 })
+
+    expect(global.setTimeout.mock.calls.length).toEqual(1)
+
+    jest.runAllTimers()
+
+    expect(db.close.mock.calls.length).toEqual(1)
+  })
+
+  test('re-use same db connection & delay auto-close', async () => {
+    const db = { close: jest.fn() }
+    SQLite.openDatabase = jest.fn(() => Promise.resolve(db))
+
+    await storage.setup({ timeout: 100 })
+    await storage.getDb()
+    await storage.getDb()
+    await storage.getDb()
+
+    expect(global.setTimeout.mock.calls.length).toEqual(4)
+
+    jest.runAllTimers()
+
+    expect(global.clearTimeout.mock.calls.length).toEqual(4)
+    expect(db.close.mock.calls.length).toEqual(1)
   })
 })
--- app/services/storage/storage.js
+++ app/services/storage/storage.js
@@ -5,9 +5,25 @@ import logger from 'ss/services/logger'
 
 const storage = () => {
   const CONNECTION_TIMEOUT_MS = 15000
-  let db, timeoutId
+  let db, timeoutId, timeout
 
-  const open = async () => {
+  const getDb = async () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    timeoutId = setTimeout(() => {
+      logger.log('Closing db connection')
+      if (db) {
+        db.close()
+      }
+      db = null
+    }, timeout)
+
+    if (db) {
+      return db
+    }
+
     const dbName = `small-steps-${Config.NODE_ENV.toLowerCase()}.db`
     db = await SQLite.openDatabase({
       androidDatabaseProvider: 'system',
@@ -18,30 +34,17 @@ const storage = () => {
     logger.log(
       `Opened db connection: '${db}'.`,
       'Lasts for at least',
-      `${CONNECTION_TIMEOUT_MS} ms`
+      `${timeout} ms`
     )
 
-    timeoutId = setTimeout(() => {
-      logger.log('Closing db connection')
-      db.close()
-      db = null
-    }, CONNECTION_TIMEOUT_MS)
-
     return db
   }
 
-  const setup = async () => {
+  const setup = async (options = {}) => {
+    timeout = options.timeout || CONNECTION_TIMEOUT_MS
     SQLite.DEBUG(Config.NODE_ENV.toLowerCase() === 'debug')
     SQLite.enablePromise(true)
-    db = await open()
-  }
-
-  const getDb = async () => {
-    if (!db) {
-      clearTimeout(timeoutId)
-      db = await open()
-    }
-    return db
+    db = await getDb()
   }
 
   const Goal = {
